70bd9f53570b8afbb6fd0529ed991d85cb8a82ae
Vladimir <vladimir@rupor.agency>
2021-06-16 19:08:04 +0300

    done task 2

---
+++ task2/script2.js
@@ -0,0 +1,11 @@
+let jsObject = {
+    name: "Anton",
+    age: 36,
+    skills: [
+        "Javascript",
+        "HTML",
+        "CSS"
+    ],
+    salary: 80000
+}
+console.log(JSON.stringify(jsObject));
